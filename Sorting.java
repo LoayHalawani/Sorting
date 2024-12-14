87bbdc92b2e25204a81cf668ad7e2287ec5e1552
Loay Halawani <loayhalawani0@gmail.com>
2024-12-14 22:57:30 +0200

    fix

--- Sorting.java
+++ Sorting.java
@@ -11,7 +11,7 @@ public class Sorting {
 	// Bubble Sort
 	private static void bubbleSort(int[] arr) {
 		for(int i = 0; i < arr.length - 1; i ++) {
-			for(int j = 0; j < arr.length - 1; j ++) {
+			for(int j = 0; j < arr.length - i - 1; j ++) {
 				if(arr[j] > arr[j + 1]) {
 					swap(arr, j, j + 1);
 				}
